c1658c8e2d4a393efdb4cc282c89f503901779e1
bryan <benevolentdictator@bryancshepherd.com>
2019-04-28 13:21:27 -0400

    Clean up bash picture taking file.

--- code/takingpictures/camera.sh
+++ code/takingpictures/camera.sh
@@ -2,29 +2,22 @@
 
 # Group number
 gn=`openssl rand -hex 3`
-echo $gn
 
 # Shutter speed
 shut=$1
-echo $shut
 
 # ISO
 isosetting=$2
-echo $isosetting
 
 # Total time in milliseconds
 totaltime=$3
-echo $totaltime
 
 # Periodicity in milliseconds
 period=$4
-echo $period
 
 # Create path and file names
 foldername=shut_${shut}_iso_${isosetting}_gn_${gn}
 imagename=$foldername
-
-echo $foldername
 
 mkdir -p ../../photos/$foldername
 
