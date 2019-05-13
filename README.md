be46a36b41d3799eab05d1c3586d104c29d71c62
Bryan <No thanks>
2019-05-12 20:07:56 -0400

    Add location tracking and fix some variable references in ./camera.sh

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ A computer vision-based project to identify roadside trash (and possibly clogged
 cd ~Projects/trashcam/code
 ./camera.sh 300 800 120000 1000
 ```
+
+### camera.sh parameters
+First parameter: Shutter exposure in milliseconds. Longer exposures mean brighter pictures, but with more blur.
+
+Second parameter: ISO (sensor sensitivity). Higher values mean brighter pictures, but with more grain. 
+
+Third parameter: Total camera time. The total amount of time we want the camera to be active.
+
+Fourth parameter: Periodicity of photos. The (approximate) amount of time between photos.
+
+The total camera time divided by the periodity of the photos is the approximate number of images that will be taken. 
 This will save pictures to the `./photos` directory
 
 ### Show a 200x200px camera preview
--- code/takingpictures/camera.sh
+++ code/takingpictures/camera.sh
@@ -10,24 +10,34 @@ randid=`openssl rand -hex 2`
 gn=${dt}_${randid}
 
 # Shutter speed
-shut=$1
+shut=${1}
 
 # ISO
-isosetting=$2
+isosetting=${2}
 
 # Total time in milliseconds
-totaltime=$3
+totaltime=${3}
 
 # Periodicity in milliseconds
 period=$4
 
 # Create path and file names
 foldername=shut_${shut}_iso_${isosetting}_gn_${gn}
-imagename=$foldername
+imagename=${foldername}
 
-mkdir -p ../../photos/staging/$foldername
+mkdir -p ../../photos/staging/${foldername}
+
+# Write the GPS coordinates to a file
+gpspipe -w -o ../../photos/staging/${foldername}/location_data.txt &
+echo 'Started location tracking'
 
 # Burst mode timelapse 
-raspistill -t $totaltime -tl $period \
--o ../../photos/staging/$foldername/$imagename_%04d.jpg \
--n -bm -ss $shut -ISO $isosetting -h 600 -w 1024
+raspistill -t ${totaltime} -tl ${period} \
+-o ../../photos/staging/${foldername}/${imagename}_%04d.jpg \
+-n -bm -ss ${shut} -ISO ${isosetting} -h 600 -w 1024
+
+# After the pictures are done, get a few more GPS data 
+# points then kill gpspipe
+echo 'Pictures done, waiting to kill location tracking'
+sleep 5
+kill %1
